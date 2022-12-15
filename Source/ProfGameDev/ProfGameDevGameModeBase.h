57b9b45b726b2d9dd5a1daf2d6f66f138b07e8d6
Matthew Layman <41220901+MatthewLayman9230@users.noreply.github.com>
2022-12-15 00:00:03 -0500

    Added SCharacter & Setup Input
    -Added Character class.
    -Added MoveForward/Turn input

---
+++ Source/ProfGameDev/ProfGameDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProfGameDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROFGAMEDEV_API AProfGameDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
